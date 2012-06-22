dfe160d7b14002e0665793f507aefac3e452cf0b
Ferdinand Pierrot <ferdinand.pierrot@telecom-paristech.fr>
2012-06-22 14:57:55 +0200

    Work in progress on the login system

---
+++ app/models/Client.java
@@ -0,0 +1,24 @@
+package models;
+
+import javax.validation.*;
+
+import play.data.validation.Constraints.*;
+
+public class Client {
+    
+    @Required
+    @MinLength(4)
+    public String login;
+    
+    @Required
+    @MinLength(6)
+    public String password;
+
+    public Client() {}
+    
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+    
+}
