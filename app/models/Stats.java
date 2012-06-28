dcb39743137d08386d1ee1b0d6b9a41eddd564db
Mehdi Belhadj <mehdi.belhadj@telecom-paristech.fr>
2012-06-28 09:44:12 +0200

    Stats Class modified to take User agent into account

--- app/models/Stats.java
+++ app/models/Stats.java
@@ -3,10 +3,12 @@ package models;
 public class Stats {
 	private int[] month;
 	private int[] year;
+	private int[] userAgent;
 	
 	public Stats() {
 		month = new int[31];
 		year = new int[12];
+		userAgent= new int[4];
 	}
 	
 	public void addToMonth(int day) {
@@ -19,7 +21,21 @@ public class Stats {
 			year[month]++;
 	}
 	
+	public void addToUserAgent(String userAgent){
+			if (userAgent.contains("Android")){
+				userAgent[0]++;				
+				}
+			else if(userAgent.contains("iPhone")){
+				userAgent[1]++;				
+				}
+			else if(userAgent.contains("Windows Phone")){
+				userAgent[2]++;				
+				}
+			else
+				userAgent[3]++;
+		}
+	
 	public String getInfo() {
-		return "today : " + month[0] + ", this month : " + year[0];
+		return "today : " + month[0] + ", this month : " + year[0]+ " , Android : "+userAgent[0];
 	}
 }
